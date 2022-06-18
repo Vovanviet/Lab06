8e2100b9e2c0f1d3eddf294a9d690fa052eba08f
Võ Văn Việt <vietvvth2107012@fpt.edu.vn>
2022-06-18 14:36:40 +0700

    update

---
+++ Lab06.1/Cooler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_1
+{
+    public class Cooler
+    {
+        public Cooler(float temperature)
+        {
+            Temperature = temperature;
+        }
+        public float Temperature
+        {
+            get { return _Temperature; }
+            set { _Temperature = value; }
+        }
+        private float _Temperature;
+        public void OnTemperatureChanged(float newTemperature)
+        {
+            if (newTemperature > Temperature)
+            {
+                System.Console.WriteLine("Cooler: On");
+            }
+            else
+            {
+                System.Console.WriteLine("Cooler: Off");
+            }
+        }
+
+    }
+}
---
+++ Lab06.1/Thermostat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_1
+{
+    public class Thermostat
+    {
+        public delegate void TemperatureChangeHandle(float newTemperature);
+        private TemperatureChangeHandle _OnTemperatureChange;
+        public TemperatureChangeHandle OnTemperatureChange
+        {
+            get { return _OnTemperatureChange; }
+            set { _OnTemperatureChange = value; }
+        }
+        public float CurrentTemperature
+        {
+            get { return _CurrentTemperature; }
+            set
+            {
+                if (value != CurrentTemperature)
+                {
+                    _CurrentTemperature = value;
+                    if (OnTemperatureChange != null)
+                    {
+                        OnTemperatureChange(value);
+                    }
+                }
+
+            }
+
+        }
+        private float _CurrentTemperature;
+
+    }
+}
